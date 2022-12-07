9954e0e0beb26912e8be28bcbb291565e7ec997a
Diego Vergara Casanova <davc93@gmail.com>
2022-12-07 12:43:06 -0300

    agregando nueva funcionalidad de arrays

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 import pandas as pd
 
 def csvTojson():
@@ -13,7 +14,24 @@ def csvTojson():
     outputData = pd.DataFrame(pd.read_csv(filepath,sep=",",header=0))
     outputData.to_json(outputPath,orient='records',date_format="epoch",double_precision=10,force_ascii=True)
 
+def csvTojsonArrays():
+    filename = input('Ingresa el nombre del archivo => ')
+    filepath = os.path.join('inputs',filename)
+    outputPath = os.path.join('outputs',f'{filename.split(sep=".")[0]}.json') 
+    data = pd.DataFrame(pd.read_csv(filepath,sep=",",header=0)).to_json(orient='records')
+    jsonFile = json.loads(data)
+    for i in jsonFile:
+        i['videos'] = i['videos'].split(";")
+        i["images[Solo si estan las imagenes subidas a lagun otro lugar]"] = i["images[Solo si estan las imagenes subidas a lagun otro lugar]"].split(";")
+    print(jsonFile)
+    print(outputPath)
+    with open(outputPath,'w') as readedFile:
+        readedFile.write(json.dumps(jsonFile))
+        readedFile.close()
+    
 
-csvTojson()
+
+
+csvTojsonArrays()
 
 
