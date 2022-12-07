398b9a31cc384e1915e3c93f4eb69665fc768adb
Diego Vergara Casanova <davc93@gmail.com>
2022-12-07 11:06:54 -0300

    paquete listo

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import os
-import csv
-import json
 import pandas as pd
 
 def csvTojson():
     filename = input('Ingresa el nombre del archivo => ')
     filepath = os.path.join('inputs',filename)
 
-    outputName = os.path.join('outputs',f'{filename.split(sep=".")[0]}.json') 
-    with open(outputName,'w') as readedFile:
+    outputPath = os.path.join('outputs',f'{filename.split(sep=".")[0]}.json') 
+    with open(outputPath,'w') as readedFile:
         readedFile.write("")
         readedFile.close()
 
     outputData = pd.DataFrame(pd.read_csv(filepath,sep=",",header=0))
-    print(outputData)
-    outputData.to_json(outputName,orient='records',date_format="epoch",double_precision=10,force_ascii=True)
-    print(outputData)
+    outputData.to_json(outputPath,orient='records',date_format="epoch",double_precision=10,force_ascii=True)
 
 
 csvTojson()
